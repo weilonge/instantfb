e037e7ca0bed17e9aa7f5f7ef7eb47870a69e2e0
Sean Lee <weilonge@gmail.com>
2018-06-15 18:01:53 +0800

    Simplify the way to find the root path in scripts.

--- utils/launch_instantfb.sh
+++ utils/launch_instantfb.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-function getSourceDirectory {
-  SOURCE="${BASH_SOURCE[0]}"
-  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  done
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  echo $DIR
-}
-
-ROOTDIR=`getSourceDirectory`
+REALDIR=`readlink -f $0`
+ROOTDIR=`dirname ${REALDIR}`
 
 ! hash node && . ~/.nvm/nvm.sh
 # nvm use node
